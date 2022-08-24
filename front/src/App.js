903be1ac797e284391e42682ec508731b888271b
Andry <andry.ramanana@epitech.eu>
2022-08-24 16:55:33 +0200

    intégration map

--- front/src/App.js
+++ front/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       {/* <div className='triangle'></div> */}
-      
+      <Home/>
     </div>
   );
 }
